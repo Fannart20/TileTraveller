ea95d3c7c17471b868a8a87488774464708fc674
Fannar Þorbergsson <fannart20@ru.is>
2020-09-17 19:15:37 +0000

    changes

--- tiletraveller.py
+++ tiletraveller.py
@@ -1,7 +1,35 @@
-def 
+def north ():
+    position += 0.1
+
+def west ():
+    position -= 1
+
+def east ():
+    position += 1
+
+def south ():
+    position -= 0.1
+
+def checkPos():
+    if position == 1.1
+        north = True
+        east, south, west = False
+    elif position == 1.2:
+        north = True
+        east = True
+        south = True
+        west = False
 
 
+
+
+
+north = True
+south = False
+west = False
+east = False
 victory = False
+position = 1.1
 
 print("You can travel:(N)orth.")
 choice = input("Direction:")
