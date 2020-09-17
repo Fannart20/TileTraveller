a9300c49a9d8041ef4b0a285a086fd52fbade4c6
ilaria20 <ilaria20@ru.is>
2020-09-17 18:58:29 +0000

    f

--- tiletraveller.py
+++ tiletraveller.py
@@ -1,2 +1,20 @@
-def room1.1 ():
-    #huh?
+def 
+
+
+victory = False
+
+print("You can travel:(N)orth.")
+choice = input("Direction:")
+while victory != True:
+    if choice != "n" or "N":
+        print("Not a valid direction!")
+    elif choice == 
+
+
+
+
+
+
+
+else: 
+    print("Victory")
